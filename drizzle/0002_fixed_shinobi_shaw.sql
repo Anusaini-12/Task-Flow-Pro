d4d030107d0e31306e690a7cb41bc6e61e76a464
Anus@ini12 <anusaini1312003@gmail.com>
2026-07-20 20:17:22 +0530

    Add migration for account table and create snapshot for database schema

---
+++ drizzle/0002_fixed_shinobi_shaw.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "account" ALTER COLUMN "updated_at" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "id_token" text;--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "access_token_expires_at" timestamp;--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "refresh_token_expires_at" timestamp;--> statement-breakpoint
+ALTER TABLE "account" ADD COLUMN "scope" text;
